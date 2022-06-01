fdfc215e84ac4a5a2073e383208346740c96bbf3
dchay <dylanhay@rogers.com>
2022-06-01 19:45:04 -0400

    game js file rev1

---
+++ assets/game.js
@@ -0,0 +1,21 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const progressBarFull = document.querySelector('#progressBarFull');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {
+        question: 'What is 2 + 2?',
+        choice1: '2', 
+        choice2: '4',
+        choice3: '22',
+        choice4: '23',
+    }
+]
