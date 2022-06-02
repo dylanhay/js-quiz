34f0259b69baceced1e3281fdae25e684efbaa00
dchay <dylanhay@rogers.com>
2022-06-01 22:41:16 -0400

    timer introduced

--- assets/game.js
+++ assets/game.js
@@ -1,111 +1,127 @@
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('#progressBarFull');
+const question = document.querySelector("#question");
+const choices = Array.from(document.querySelectorAll(".choice-text"));
+const progressText = document.querySelector("#progressText");
+const scoreText = document.querySelector("#score");
+const progressBarFull = document.querySelector("#progressBarFull");
+// const timerNum = document.querySelector("#timer-num");
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+var timeleft = 100;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("timer-num").innerHTML = "Finished";
+  } else {
+    document.getElementById("timer-num").innerHTML = timeleft;
+  }
+  timeleft -= 1;
+}, 1000);
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+let timer = 100;
 
 let questions = [
-    {
-        question: 'Commonly used data types do not include:',
-        choice1: 'strings', 
-        choice2: 'booleans',
-        choice3: 'alerts',
-        choice4: 'numbers',
-        answer: 3,
-    },
-    {
-        question: 'The condition in an if/else statement is enclosed with:',
-        choice1: 'quotes', 
-        choice2: 'curly brackets',
-        choice3: 'parentheses',
-        choice4: 'square brackets',
-        answer: 3,
-    },
-    {
-        question: 'A very useful tool for printing content to the debugger during development and debugging is',
-        choice1: 'JavaScript', 
-        choice2: 'terminal/bash',
-        choice3: 'for loops',
-        choice4: 'console.log',
-        answer: 4,
-    },
-    {
-        question: 'Arrays in JavaScript can be used to store:',
-        choice1: 'numbers and strings', 
-        choice2: 'other arrays',
-        choice3: 'booleans',
-        choice4: 'all of the above',
-        answer: 4,
-    },
-]
+  {
+    question: "Commonly used data types do not include:",
+    choice1: "strings",
+    choice2: "booleans",
+    choice3: "alerts",
+    choice4: "numbers",
+    answer: 3,
+  },
+  {
+    question: "The condition in an if/else statement is enclosed with:",
+    choice1: "quotes",
+    choice2: "curly brackets",
+    choice3: "parentheses",
+    choice4: "square brackets",
+    answer: 3,
+  },
+  {
+    question:
+      "A very useful tool for printing content to the debugger during development and debugging is",
+    choice1: "JavaScript",
+    choice2: "terminal/bash",
+    choice3: "for loops",
+    choice4: "console.log",
+    answer: 4,
+  },
+  {
+    question: "Arrays in JavaScript can be used to store:",
+    choice1: "numbers and strings",
+    choice2: "other arrays",
+    choice3: "booleans",
+    choice4: "all of the above",
+    answer: 4,
+  },
+];
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 4
+const SCORE_POINTS = 100;
+const MAX_QUESTIONS = 4;
 
 startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
-}
+  questionCounter = 0;
+  score = 0;
+  timerNum = 100;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
 
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score)
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
 
-        return window.location.assign('/end.html')
+    return window.location.assign("/end.html");
+  }
+
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionsIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionsIndex, 1);
+
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    let classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      incrementScore(SCORE_POINTS);
     }
 
-    questionCounter++
-    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
+    selectedChoice.parentElement.classList.add(classToApply);
 
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
 
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
+incrementScore = (num) => {
+  score += num;
+  scoreText.innerText = score;
+};
 
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
-}
-
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return 
-
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
-
-        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
-
-        if(classToApply === 'correct'){
-            incrementScore(SCORE_POINTS)
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply)
-
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-        }, 1000)
-    } )
-})
-
-incrementScore = num => {
-    score +=num
-    scoreText.innerText = score
-}
-
-startGame()
+startGame();
